16595496176749c762e99c71f0f0513b02fd5483
mob <innermond@yahoo.com>
2017-09-26 19:02:23 +0300

    fix

--- public/page/menu/nav.php
+++ public/page/menu/nav.php
@@ -25,7 +25,7 @@ if (is_array($params) and ! empty($params)) {
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-					<a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo"></a> </div>
+					<a class="navbar-brand" href="/"><img src="images/logo.png" alt="logo"></a> </div>
 				
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
