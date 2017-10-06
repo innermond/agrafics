16b42e6d4d77928aa732638403a464bc5fbf70a0
mob <innermond@yahoo.com>
2017-10-06 16:57:29 +0300

    favico

--- public/page/head.php
+++ public/page/head.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta charset="utf-8">
 <title>Agrafics - <?=$params[0]?></title>
 <!-- Bootstrap -->
 <link href="/css/bootstrap.min.css" rel="stylesheet">
